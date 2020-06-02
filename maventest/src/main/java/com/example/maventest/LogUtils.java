0d00bb43ed60d35dba384c008977037e8179a413
leiwei <343622588@qqcom>
2020-06-02 19:12:09 +0800

    设置为可透传

--- maventest/src/main/java/com/example/maventest/LogUtils.java
+++ maventest/src/main/java/com/example/maventest/LogUtils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.widget.Toast;
 
 import com.alivc.rtc.AliRtcEngine;
+import com.tencent.ilivesdk.ILiveCallBack;
+import com.tencent.ilivesdk.ILiveSDK;
 
 
 public class LogUtils {
@@ -11,6 +13,13 @@ public class LogUtils {
     public static AliRtcEngine showLongTwo(Context context, String text) {
 //        safeShow(context, text, Toast.LENGTH_LONG);
         AliRtcEngine aliRtcEngine =  AliRtcEngine.getInstance(context);
+
+        return aliRtcEngine;
+    }
+
+    public static ILiveSDK showLongOne(Context context, String text) {
+//        safeShow(context, text, Toast.LENGTH_LONG);
+        ILiveSDK aliRtcEngine =  ILiveSDK.getInstance();
 
         return aliRtcEngine;
     }
