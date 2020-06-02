1681b48f3c3045cdb48c2532686ee6cc642fc6ca
leiwei <343622588@qqcom>
2020-06-02 14:33:35 +0800

    第一版

---
+++ maventest/src/main/java/com/example/maventest/LogUtils.java
@@ -0,0 +1,11 @@
+package com.example.maventest;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class LogUtils {
+
+    public static void showLongTwo(Context context, String text) {
+//        safeShow(context, text, Toast.LENGTH_LONG);
+    }
+}
