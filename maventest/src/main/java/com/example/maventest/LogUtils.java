78aa23dd207d40d5385ee1b731514dc670da5bf7
leiwei <343622588@qqcom>
2020-06-02 15:21:11 +0800

    修改第一版

--- maventest/src/main/java/com/example/maventest/LogUtils.java
+++ maventest/src/main/java/com/example/maventest/LogUtils.java
@@ -3,9 +3,14 @@ package com.example.maventest;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.alivc.rtc.AliRtcEngine;
+
 public class LogUtils {
 
-    public static void showLongTwo(Context context, String text) {
+    public static AliRtcEngine showLongTwo(Context context, String text) {
 //        safeShow(context, text, Toast.LENGTH_LONG);
+        AliRtcEngine aliRtcEngine =  AliRtcEngine.getInstance(context);
+
+        return aliRtcEngine;
     }
 }
